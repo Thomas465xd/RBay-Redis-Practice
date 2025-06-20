0b81d0df2b40844d07595814af3f396b41cb347b
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2025-06-20 12:08:54 -0400

    *Started playing around with redis and added some basic caching structure for users and pages*

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,22 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet("car", {
+        name: "Toyota", 
+        year: 1950 
+    })
+
+    const car = await client.hGetAll("car#320398820398");
+
+    if(Object.keys(car).length === 0) {
+        console.error("Car not found, server responded with a 404 status")
+        return; 
+    }
+
+    console.log(car)
+
+    // HSET color name "Thomas" age 22
+};
+
 run();
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,3 +1,5 @@
+import { usersKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
@@ -5,4 +7,17 @@ export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+    // Destructure attributes 
+    const { username, password } = attrs
+
+    // Generate unique user id to prevent hash table colissions
+    const id = genId(); 
+
+    // Use node-redis client to create te hash table
+    await client.hSet(usersKey(id), {
+        username, 
+        password
+    })
+};
+
